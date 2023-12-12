66c6433859e3478bb4745da8bb84fb0355ae90c3
giannis korkontzilas <gianniskorkontzilas@hotmail.com>
2023-12-12 02:36:09 +0200

    Main updated

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -2,7 +2,7 @@ package gr.aueb.cf.proj5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello CF!!");
+        System.out.println("Hello Coding Factory!!!!!!!!!!");
 
     }
 }
